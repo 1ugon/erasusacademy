097bce04fb67abc55f72afc969495b253635a0d8
Pedro Lugon <pedromotta20@gmail.com>
2021-12-14 05:46:16 -0300

    infos

--- src/sections/Infos.tsx
+++ src/sections/Infos.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-import info1 from "../assets/infos/info1.png";
+import info1 from "../assets/infos/info1.jpg";
+import info2 from "../assets/infos/info2.jpg";
+import info3 from "../assets/infos/info3.jpg";
+import info4 from "../assets/infos/info4.jpg";
+import info5 from "../assets/infos/info5.jpg";
+import info6 from "../assets/infos/info6.jpg";
+import info7 from "../assets/infos/info7.jpg";
 
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -14,13 +20,14 @@ function Infos() {
     <div className={styles.containerWhite}>
       <h1 className={styles.title}>O QUE VOU APRENDER?</h1>
       <div className={styles.containerInfosCards}>
-        <Carousel infiniteLoop showStatus={false}>
-          <div>
-            <img src={info1} className={styles.feedImage} alt="feedback one" />
-          </div>
-          <div>
-            <img src={info1} className={styles.feedImage} alt="feedback two" />
-          </div>
+        <Carousel infiniteLoop showStatus={false} showIndicators={false}>
+          <img src={info1} className={styles.feedImage} alt="info 1" />
+          <img src={info2} className={styles.feedImage} alt="info 2" />
+          <img src={info3} className={styles.feedImage} alt="info 3" />
+          <img src={info4} className={styles.feedImage} alt="info 4" />
+          <img src={info5} className={styles.feedImage} alt="info 5" />
+          <img src={info6} className={styles.feedImage} alt="info 6" />
+          <img src={info7} className={styles.feedImage} alt="info 7" />
         </Carousel>
         <Button text="QUERO APRENDER" />
       </div>
