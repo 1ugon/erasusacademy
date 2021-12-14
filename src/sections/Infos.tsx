9b9bba9632bf7db1a19b402559097f15153bc01a
Pedro Lugon <pedromotta20@gmail.com>
2021-12-14 06:01:10 -0300

    fixing

--- src/sections/Infos.tsx
+++ src/sections/Infos.tsx
@@ -11,16 +11,25 @@ import info7 from "../assets/infos/info7.jpg";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+import { useWindowSize } from "react-use";
+
 import Button from "../components/Button";
 
 import styles from "../styles/White.module.css";
 
 function Infos() {
+  const { width } = useWindowSize();
+
   return (
     <div className={styles.containerWhite}>
       <h1 className={styles.title}>O QUE VOU APRENDER?</h1>
       <div className={styles.containerInfosCards}>
-        <Carousel infiniteLoop showStatus={false} showIndicators={false}>
+        <Carousel
+          infiniteLoop
+          showStatus={false}
+          showIndicators={false}
+          showThumbs={width > 1080 ? true : false}
+        >
           <img src={info1} className={styles.feedImage} alt="info 1" />
           <img src={info2} className={styles.feedImage} alt="info 2" />
           <img src={info3} className={styles.feedImage} alt="info 3" />
@@ -29,8 +38,8 @@ function Infos() {
           <img src={info6} className={styles.feedImage} alt="info 6" />
           <img src={info7} className={styles.feedImage} alt="info 7" />
         </Carousel>
-        <Button text="QUERO APRENDER" />
       </div>
+      <Button text="QUERO APRENDER" />
     </div>
   );
 }
